ef218e8838d874096fd0737a70a4d293c568a127
JGSilva <jgoliveira445@gmail.com>
2024-02-15 09:39:29 -0300

    fix: alteração do backup

--- backup.py
+++ backup.py
@@ -1,5 +1,17 @@
 import cv2
-import numpy as np
+
+# Função de callback para evento de clique
+def click_event(event, x, y, flags, params):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # Calcula a coluna e a linha da casa clicada
+        col_clicked = x // square_size_x
+        row_clicked = 7 - (y // square_size_y)  # Inverte a ordem das linhas
+        # Converte o índice da coluna para a letra correspondente (A-H)
+        col_name = chr(65 + col_clicked)
+        # Converte o índice da linha para o número correspondente (1-8)
+        row_name = str(row_clicked + 1)  # Adiciona 1 ao índice da linha
+        # Exibe as coordenadas da casa clicada
+        print(f"Casa clicada: {col_name}{row_name}")
 
 # Carrega a imagem do tabuleiro de xadrez
 image = cv2.imread('tabuleiro.png')
@@ -8,7 +20,7 @@ image = cv2.imread('tabuleiro.png')
 rows = 8
 cols = 8
 
-# Calcula o tamanho aproximado de cada quadrado do tabuleiro
+# Calcula o tamanho exato de cada quadrado do tabuleiro
 square_size_x = image.shape[1] // cols
 square_size_y = image.shape[0] // rows
 
@@ -25,23 +37,26 @@ for row in range(rows):
         col_name = chr(65 + col)  # Converte o valor da coluna para a letra correspondente (A-H)
         row_name = str(8 - row)    # Converte o valor da linha para o número correspondente (1-8)
         # Desenha um quadrado delimitando a casa
-        cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        # cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
         # Calcula o centro do quadrado
         cx = (x1 + x2) // 2
         cy = (y1 + y2) // 2
         # Desenha um círculo nas coordenadas da casa
-        cv2.circle(image, (cx, cy), 5, (0, 255, 0), -1)
+        # cv2.circle(image, (cx, cy), 5, (0, 255, 0), -1)
         # Escreve as coordenadas da casa
-        cv2.putText(image, f'{col_name}{row_name}', (cx - 10, cy - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        cv2.putText(image, f'{col_name}{row_name}', (cx - 10, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-# Redimensiona a imagem para 60% do seu tamanho original
-scale_percent = 60  
-width = int(image.shape[1] * scale_percent / 100)
-height = int(image.shape[0] * scale_percent / 100)
+# Redimensiona a imagem
+width = int(image.shape[1])
+height = int(image.shape[0])
 dim = (width, height)
 resized_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
 # Exibe a imagem com as coordenadas das casas e os quadrados delimitando cada casa
 cv2.imshow('Chess Board', resized_image)
+
+# Define o evento de clique do mouse
+cv2.setMouseCallback('Chess Board', click_event)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
